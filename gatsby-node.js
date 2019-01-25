0c822b0f74210c1d7cc9b4e523ca661ec543e525
iwfan <i.wangfancn@gmail.com>
2019-01-25 18:13:11 +0800

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,12 +6,23 @@ exports.onCreateWebpackConfig = (
   { stage, actions, plugins, getConfig, rules },
   { rule: ruleProps = {} },
 ) => {
-  actions.setWebpackConfig({
+  const isDevelop = stage === `develop` || stage === `develop-html`;
+
+  const webpackConfig = {
     resolve: {
       alias: {
         '@': resolve('src'),
         '@svg': resolve('static/svg'),
       },
     },
-  });
+  };
+
+  if (isDevelop) {
+    // adds sourcemaps for tsx in dev mode
+    Object.assign(webpackConfig, {
+      devtool: 'eval-source-map',
+    });
+  }
+
+  actions.setWebpackConfig(webpackConfig);
 };
