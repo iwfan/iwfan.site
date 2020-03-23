5f5c44ddc89b6411b543bb00007f757b421cbbcd
iwfan <i.wangfancn@gmail.com>
2020-03-23 22:34:20 +0800

    clear

--- gatsby-node.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-require('source-map-support').install();
-require('ts-node').register();
-
-exports.onCreateWebpackConfig = require('./src/lib/onCreateWebpackConfig');
-exports.onCreateNode = require('./src/lib/onCreateNode');
-exports.createPages = require('./src/lib/createPages');
--- src/styles/colors.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  primary_text: '#38d39f',
-  text: '#424647',
-};
