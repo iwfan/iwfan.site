fd94bef2ad63e95d40152721e6bdb8c444600c53
iwfan <i.wangfancn@gmail.com>
2020-05-24 18:21:22 +0800

    update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -1,7 +1,10 @@
 echo "yarn add chrome-aws-lambda"
 yarn add chrome-aws-lambda
+echo "before \n\n"
 head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
-sed -i '1i\const chrome = require('chrome-aws-lambda');' ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+sed -i '1i\const chrome = require(\'chrome-aws-lambda\');' ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+echo "after1 \n\n"
 head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
 sed -i 's/puppeteer.launch();/puppeteer.launch({args: chrome.args,executablePath: await chrome.executablePath,headless:chrome.headless,});/' ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+echo "after2 \n\n"
 head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
