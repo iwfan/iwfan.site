c309cd7332b6e22ab641ceb50c40d86315690dff
iwfan <i.wangfancn@gmail.com>
2020-05-24 18:04:59 +0800

    - add prebuild script

--- pre_build.sh
+++ pre_build.sh
@@ -1,4 +1,5 @@
-echo $CI
+echo "yarn add chrome-aws-lambda"
 yarn add chrome-aws-lambda
+head -n 10 /node_modules/gatsby-source-notion-database/src/getPageHtml.js
 sed -i '' '1i\const chrome = require('chrome-aws-lambda');' /node_modules/gatsby-source-notion-database/src/getPageHtml.js
-cat /node_modules/gatsby-source-notion-database/src/getPageHtml.js
+head -n 10 /node_modules/gatsby-source-notion-database/src/getPageHtml.js
