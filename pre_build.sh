1c175c9a606732dca80f8e180219794ad57e8ad3
iwfan <i.wangfancn@gmail.com>
2020-05-24 18:06:43 +0800

    - fix error path

--- pre_build.sh
+++ pre_build.sh
@@ -1,5 +1,5 @@
 echo "yarn add chrome-aws-lambda"
 yarn add chrome-aws-lambda
-head -n 10 /node_modules/gatsby-source-notion-database/src/getPageHtml.js
-sed -i '' '1i\const chrome = require('chrome-aws-lambda');' /node_modules/gatsby-source-notion-database/src/getPageHtml.js
-head -n 10 /node_modules/gatsby-source-notion-database/src/getPageHtml.js
+head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+sed -i '' '1i\const chrome = require('chrome-aws-lambda');' ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
