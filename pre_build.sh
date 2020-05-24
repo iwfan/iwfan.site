e0c859b58687cd5f655e5c78f07d609e68912fb6
iwfan <i.wangfancn@gmail.com>
2020-05-24 20:17:35 +0800

    update pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -6,7 +6,7 @@ head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
 sed -i "1i\const chrome = require('chrome-aws-lambda');" ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
 echo "after1 \n\n"
 head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
-sed -i "s/require('puppeteer')/require('puppeteer-core')/" ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
-sed -i 's/puppeteer.launch();/puppeteer.launch({args: chrome.args,executablePath: await chrome.executablePath,headless:chrome.headless,});/' ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+#sed -i "s/require('puppeteer')/require('puppeteer-core')/" ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
+sed -i 's/puppeteer.launch();/puppeteer.launch({args: chrome.args,executablePath: await chrome.executablePath,headless:true,});/' ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
 echo "after2 \n\n"
 head -n 10 ./node_modules/gatsby-source-notion-database/src/getPageHtml.js
