01ab6b544ea498d943febf4cddcd1d12097f875a
iwfan <i.wangfancn@gmail.com>
2020-07-26 23:14:24 +0800

    update create new post script

--- libs/new_post.ts
+++ libs/new_post.ts
@@ -1,18 +1,18 @@
 import fs from 'fs'
 import path from 'path'
-import { v1 as uuid } from 'uuid'
+import md5 from 'blueimp-md5';
 import { post_dir } from '../blog.config'
 
 const posts_dir = path.resolve(__dirname, '..', post_dir)
-const today = new Date()
+const today = new Date().toISOString()
 const file_name = process.argv[2] ?? `untitled`
-const post_id = uuid({ msecs: today.getTime() })
+const post_id = md5(today);
 
 const front_matter = [
   '---',
   `title: ${file_name}`,
   `slug: ${post_id}`,
-  `data: ${today.toISOString()}`,
+  `data: ${today}`,
   `tags:`,
   `  - Untagged`,
   `thumbnail: null`,
@@ -24,5 +24,4 @@ const markdown_file = path.resolve(posts_dir, `${file_name}.md`)
 fs.writeFile(markdown_file, front_matter.join('\n'), { flag: 'wx' }, err => {
   console.log(err ? err.message : `Created ${markdown_file}`)
 })
-
 
