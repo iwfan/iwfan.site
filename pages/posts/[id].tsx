3379e1e555c29e49672c330d49c7d42901edb31b
iwfan <i.wangfancn@gmail.com>
2022-03-20 23:13:07 +0800

    feat: remove page title

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -14,22 +14,22 @@ const Posts: NextPage<NotionPageBlock> = props => {
 
   const { recordMap } = props
 
-  const keys = Object.keys(recordMap?.block || {})
-  const block = recordMap?.block?.[keys[0]]?.value
-
-  const postTitle = getBlockTitle(block, recordMap) || site_title
-
-  if (postTitle == null) {
-    return null
-  }
+  // const keys = Object.keys(recordMap?.block || {})
+  // const block = recordMap?.block?.[keys[0]]?.value
+  //
+  // const postTitle = getBlockTitle(block, recordMap) || site_title
+  //
+  // if (postTitle == null) {
+  //   return null
+  // }
 
   return (
     <>
-      <Head>
-        <title>
-          {postTitle} | {site_title}
-        </title>
-      </Head>
+      {/* <Head> */}
+      {/*   <title> */}
+      {/*     {postTitle} | {site_title} */}
+      {/*   </title> */}
+      {/* </Head> */}
       <Layout>
         <article
           className="text-grey leading-relaxed dark-mode"
@@ -40,10 +40,10 @@ const Posts: NextPage<NotionPageBlock> = props => {
             } as React.CSSProperties
           }
         >
-          <header className="my-12">
-            <h1 className="my-2 text-3xl font-bold text-fg">{postTitle}</h1>
-            {/* <p className="text-blue">{date}</p> */}
-          </header>
+          {/* <header className="my-12"> */}
+          {/*   <h1 className="my-2 text-3xl font-bold text-fg">{postTitle}</h1> */}
+          {/*   <p className="text-blue">{date}</p> */}
+          {/* </header> */}
           <NotionRenderer
             recordMap={props.recordMap}
             fullPage={true}
