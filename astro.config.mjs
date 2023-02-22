e72bf2cda955e2e67e84914533c4719845c7827c
iwfan <i.wangfancn@gmail.com>
2023-02-22 21:05:38 +0800

    feat: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-/* import { dirname, resolve } from 'path'; */
-/* import { fileURLToPath } from 'url'; */
-/**/
-/* const __dirname = dirname(fileURLToPath(import.meta.url)); */
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind({
-     config: { path: './tailwind.config.mjs' },
-  })]
-});
+  site: "https://iwfan.site",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      config: { path: "./tailwind.config.mjs" }
+    })
+  ]
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,61 +1,40 @@
-import typography from '@tailwindcss/typography'
+import typography from "@tailwindcss/typography"
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      fontFamily: {
-        Jost: [
-          'Jost',
-          'Lato',
-          'Geneva',
-          'ui-sans-serif',
-          'system-ui',
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica Neue',
-          'Arial',
-          'Noto Sans',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-          'Noto Color Emoji',
-        ],
-      },
       colors: {
-        fg: '#ECF8FF',
-        green: '#83F9A2',
-        grey: '#C9E2F0',
-        blue: '#5E8CA7',
-        border: 'rgb(28, 44, 53)',
-        border_light: 'rgba(124, 201, 255, 0.41)',
-        light: 'rgba(130, 249, 161, 0.07)',
-        bar: 'rgb(222, 255, 231)',
-        link: '#4FB8FF',
-        ['deep-green']: 'rgb(22, 33, 41)',
+        fg: "#ECF8FF",
+        green: "#83F9A2",
+        grey: "#C9E2F0",
+        blue: "#5E8CA7",
+        border: "rgb(28, 44, 53)",
+        border_light: "rgba(124, 201, 255, 0.41)",
+        light: "rgba(130, 249, 161, 0.07)",
+        bar: "rgb(222, 255, 231)",
+        link: "#4FB8FF",
+        ["deep-green"]: "rgb(22, 33, 41)"
       },
       boxShadow: {
-        emanate: 'rgb(14 255 77 / 50%) 3px 0px 22px',
+        emanate: "rgb(14 255 77 / 50%) 3px 0px 22px"
       },
       keyframes: {
         expand: {
-          '0%': { transform: 'scale(1.1)' },
-          '100%': { transform: 'scale(1)' },
+          "0%": { transform: "scale(1.1)" },
+          "100%": { transform: "scale(1)" }
         },
         shrink: {
-          '0%': { transform: 'scale(0.9)' },
-          '100%': { transform: 'scale(1)' },
-        },
+          "0%": { transform: "scale(0.9)" },
+          "100%": { transform: "scale(1)" }
+        }
       },
       animation: {
-        expand: 'expand 1000ms infinite alternate-reverse',
-        shrink: 'shrink 1000ms infinite alternate-reverse',
-      },
-    },
+        expand: "expand 1000ms infinite alternate-reverse",
+        shrink: "shrink 1000ms infinite alternate-reverse"
+      }
+    }
   },
-  plugins: [typography],
+  plugins: [typography]
 }
