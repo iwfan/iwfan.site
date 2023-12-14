f127adcb9d55fb8e6d33c545707c0b006205b0f0
iwfan <iwfan@duck.com>
2023-12-14 09:11:37 +0800

    improve landing page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         jost: "Jost, sans-serif",
+        lxgw: "'LXGW WenKai Screen', sans-serif",
       },
       colors: {
         glow10: "#B0FBBC",
@@ -16,6 +17,7 @@ export default {
         text20: "#C9E2F0",
         text30: "#BBC6CC",
         blue10: "#111A20",
+        blue15: "#19252E",
         blue20: "#1C2C35",
         blue30: "#243B4A",
         blue40: "#416883",
@@ -29,20 +31,6 @@ export default {
       },
       boxShadow: {
         emanate: "rgb(14 255 77 / 50%) 3px 0px 22px",
-      },
-      keyframes: {
-        expand: {
-          "0%": { transform: "scale(1.1)" },
-          "100%": { transform: "scale(1)" },
-        },
-        shrink: {
-          "0%": { transform: "scale(0.9)" },
-          "100%": { transform: "scale(1)" },
-        },
-      },
-      animation: {
-        expand: "expand 1000ms infinite alternate-reverse",
-        shrink: "shrink 1000ms infinite alternate-reverse",
       },
     },
   },
