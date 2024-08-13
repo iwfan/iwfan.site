d3f82a8193a882a33e3c4eddf442ffce3059bab3
iwfan <iwfan@duck.com>
2024-08-13 18:22:40 +0800

    ditch backup things, it's useless

--- backup/components/notion/renderer/Divider.tsx
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react'
-
-const Divider: FC = () => <hr className="my-2 border-border border-t-2 border-dashed" />
-
-export default Divider
--- backup/components/notion/x.ts
+++
@@ -1,8 +0,0 @@
-import { NotionAPI } from 'notion-client'
-
-const notion = new NotionAPI()
-
-export const retrieveNotionPage = async (pageId: string) => {
-  const recordMap = await notion.getPage(pageId)
-  return { recordMap }
-}
